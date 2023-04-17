701f1d17c6bbdad173c43166c518140b98d175ad
a0x96abfut <129926898+a0x96abfut@users.noreply.github.com>
2023-04-17 03:26:35 -0400

    Update install-mongo.sh

--- install-mongo.sh
+++ install-mongo.sh
@@ -1,3 +1,16 @@
 clear
 echo "### PULL MongoDB container ###"
 sudo podman pull docker.io/library/mongo:latest
+
+echo "### INSTALLING Debian Dependencies ###"
+apt-get install gnupg wget -y
+
+curl -fsSL https://pgp.mongodb.com/server-6.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg \
+   --dearmor
+
+echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg] http://repo.mongodb.org/apt/debian bullseye/mongodb-org/6.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+
+sudo apt-get update
+
+sudo apt-get install -y mongodb-org
